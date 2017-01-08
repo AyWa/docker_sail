0780f52b11f7b6c9398f497e2964cf4ac48b911f
hurabielle marc <marc.hurabielle@gmail.com>
2017-01-08 13:20:02 +0000

    final

--- neo4j_doc_manager/script_entrance_neo4j_doc_manager.sh
+++ neo4j_doc_manager/script_entrance_neo4j_doc_manager.sh
@@ -3,5 +3,5 @@ echo "yolo le docker";
 export NEO4J_AUTH=neo4j:sail
 echo $NEO4J_AUTH
 env | grep NEO4J
-mongo-connector -m mongodb://sailroot:Sail2017@172.18.0.3:27017/admin -t http://172.17.0.3:7474/db/data -d neo4j_doc_manager
+mongo-connector -m mongodb://sailroot:Sail2017@mongo2:27017/admin -t http://neo4j_server:7474/db/data -d neo4j_doc_manager
 exit 0
