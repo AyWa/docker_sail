5f058d2b6f4a055a3de55e0031fc88a6ff1b0135
hurabielle marc <marc.hurabielle@gmail.com>
2017-01-13 10:18:30 +0000

    test sailapp

---
+++ sail_app/script_entrance_sail_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eu
+rm -r SAIL/
+GIT_SSH_COMMAND='ssh -i /root/.ssh/id_rsa' git clone git@github.com:Ciboulette/SAIL.git
+cd SAIL/
+npm install
+cd app/
+npm install
+npm run build
+cd ..
+pm2 start server.js
+exit 0
