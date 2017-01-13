7aa13d4897b50ad7475c4c3cdc8aec13c0510d0e
hurabielle marc <marc.hurabielle@gmail.com>
2017-01-13 12:44:35 +0000

    docker finished

--- sail_app/script_entrance_sail_app.sh
+++ sail_app/script_entrance_sail_app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eu
+rm -rf SAIL
 GIT_SSH_COMMAND='ssh -i /root/.ssh/id_rsa' git clone git@github.com:Ciboulette/SAIL.git
 cd SAIL/
 npm install
@@ -6,5 +7,9 @@ cd app/
 npm install
 npm run build
 cd ..
+sed -i -e "s/198.211.118.218:30001/mongo1:27017/g" config/database.js
+sed -i -e "s/localhost:4200/198.211.118.218:4200/g" config/database.js
+sed -i -e "s/198.211.118.218/neo4j_server/g" neo4j/user.js
 pm2 start server.js
+pm2 logs
 exit 0
