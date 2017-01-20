42452823dc5fbc60743614efcb74db1a3a8f67ef
hurabielle marc <marc.hurabielle@gmail.com>
2017-01-20 15:04:21 +0000

    edit server adress

--- sail_app/script_entrance_sail_app.sh
+++ sail_app/script_entrance_sail_app.sh
@@ -4,12 +4,14 @@ GIT_SSH_COMMAND='ssh -i /root/.ssh/id_rsa' git clone git@github.com:Ciboulette/S
 cd SAIL/
 npm install
 cd app/
+sed -i -e "s/localhost:4200/198.211.118.218:4200/g" src/app/notifications/notification.service.ts
 npm install
 npm run build
 cd ..
 sed -i -e "s/198.211.118.218:30001/mongo1:27017/g" config/database.js
 sed -i -e "s/localhost:4200/198.211.118.218:4200/g" config/database.js
 sed -i -e "s/198.211.118.218/neo4j_server/g" neo4j/user.js
+sed -i -e "s/localhost:4200/198.211.118.218:4200/g" emails/email.js
 pm2 start server.js
 pm2 logs
 exit 0
